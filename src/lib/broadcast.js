bfbe036b1b2acb3ed1d79a67bf4395b6c64707fb
Thanh Vuong <thanhvg@gmail.com>
2020-08-16 23:40:12 -0600

    improve hook

--- src/lib/broadcast.js
+++ src/lib/broadcast.js
@@ -1,23 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from "react";
 
 export function createBroadcast(val) {
   const listeners = new Set();
-  return function () {
+  const setter = v => {
+    listeners.forEach(it => it(v));
+  };
+  return function() {
     // XXX is setSate stable accross all renders?
-    // yes 
+    // yes
     // https://reactjs.org/docs/hooks-reference.html React guarantees that
     // setState function identity is stable and won’t change on re-renders. This
     // is why it’s safe to omit from the useEffect or useCallback dependency
     // list.
-    
     const [state, setSate] = useState(val);
-    listeners.add(setSate);
-    const setter = (v) => {
-      listeners.forEach(it => it(v))
-    }
-    useEffect(() => () => {
-      listeners.delete(setSate);
-    }, [])
+    useEffect(() => {
+      listeners.add(setSate);
+      return () => {
+        listeners.delete(setSate);
+      };
+    }, []);
     return [state, setter];
-  }
+  };
 }
